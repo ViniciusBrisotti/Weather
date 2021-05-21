f24d5c9057c537c39221e5e6e2f536275d7b4779
ViniciusBrisotti <vinicius.brisotti@hotmail.com>
2021-05-21 16:10:07 -0300

    Deleting unnecessary comments

--- src/main/java/weather/weather/controller/ApiController.java
+++ src/main/java/weather/weather/controller/ApiController.java
@@ -7,23 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import weather.weather.api.MetaWeatherAPI;
 import weather.weather.custom.CityName;
-
-
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class ApiController {
 
-
     private final MetaWeatherAPI metaWeatherAPI;
 
-//    @GetMapping("/")
-//    public String test() {
-//        List<CityName> london = metaWeatherAPI.cityName("london");
-//        return london.toString();
-//
-//    }
 @GetMapping("/{city}")
 @ResponseBody
 public List<CityName> test(@PathVariable("city") String cityName) {
@@ -34,6 +25,7 @@ public List<CityName> test(@PathVariable("city") String cityName) {
             //Chamar o método  citywoeid com a variável criada acima
             //Mapear o retorno do citywoeid em um objeto
         //  Os campos que coloquei na WOEID nao sao os que esta devolvendo
+
     return cityNames;
 }
 
